99400744d887395034096b62ef8a9c17a9f21f11
philopian <pdotwdot@gmail.com>
2017-08-09 18:43:50 -0700

    Storybook Info Addon

--- stories/index.js
+++ stories/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
-import { storiesOf } from '@storybook/react';
+import { storiesOf, configure, setAddon } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { withInfo } from '@storybook/addon-info';
 
 
 import HelloWorld from '../src/components/HelloWorld.jsx';
 storiesOf('HelloWorld', module)
-  .add('Default', () => (
-    <HelloWorld />
-  ))
-  .add('custom message', () => (
-    <HelloWorld message='custom message' />
-  ))
+  .add('Default',
+    withInfo('doc string about my component')(() =>
+      <HelloWorld />
+    )
+  )
+  .add('Custom message',
+    withInfo('doc string about my component')(() =>
+      <HelloWorld message='custom message' />
+    )
+  );
